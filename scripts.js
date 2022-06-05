4cab904781f18ee61da0958e09e7c9ba71374a09
NikaNika <55001607+BigRaccoon@users.noreply.github.com>
2022-06-05 13:49:57 +0300

    add visual information

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ for (let i = 0; i < NumberOfTicket; i++) {
   counter[i] = i + 1;
 }
 
-const numberOfMember = parseInt(prompt("Введите число даунов"));
+const numberOfMember = parseInt(prompt("Введите число участников"));
 
 let membersArray = [];
 for (let i = 0; i < numberOfMember; i++) {
@@ -22,7 +22,25 @@ for (let i = 1; i <= counter.length; i++) {
   counterForMembersArray++;
 }
 
-for (let i = 0; i <= membersArray.length; i++) {
+let numberOfQ = document.querySelector(".numberOfQ");
+numberOfQ.innerHTML = `Количество вопросов: ${NumberOfTicket}`;
+
+let numberOfM = document.querySelector(".numberofM");
+numberOfM.innerHTML = `Количество участников: ${numberOfMember}`;
+
+let tableInfo = document.querySelector(".table");
+for (let i = 0; i < numberOfMember; i++) {
+  let stringTr = document.createElement("tr");
+  let columnTdName = document.createElement("td");
+  columnTdName.innerHTML = `${membersArray[i].name}`;
+  let columnTdQuest = document.createElement("td");
+  columnTdQuest.innerHTML = `${membersArray[i].qustions}`;
+  stringTr.appendChild(columnTdName);
+  stringTr.appendChild(columnTdQuest);
+  tableInfo.appendChild(stringTr);
+}
+
+for (let i = 0; i < membersArray.length; i++) {
   console.log(
     `Вопросы для ${membersArray[i].name}: ${membersArray[i].qustions}`
   );
